c942221a728d2f5dc32e5c79ab68384091bd3c9d
Jerome Ortega <Zerospace@Jeromes-MacBook-Pro.local>
2016-11-22 12:49:32 -0800

    Added Google Geolocation API and Used Lat & Lon in Yelp Query

--- apps/hater_app/views.py
+++ apps/hater_app/views.py
@@ -5,10 +5,10 @@ from yelp.oauth1_authenticator import Oauth1Authenticator
 
 
 auth = Oauth1Authenticator(
-	consumer_key = sKWWP86fBkphMtLTYINRdQ,
-	consumer_secret = ZdoDvJCzdbZ60y7OPNfLIVtcdhE,
-    token = hF6LiItqZwotEi-pZghzcxLsGf1N3p5P,
-    token_secret = ETi02p-5lO2hKM5bBACayb-cX_w
+	consumer_key = 'sKWWP86fBkphMtLTYINRdQ',
+	consumer_secret = 'ZdoDvJCzdbZ60y7OPNfLIVtcdhE',
+    token = 'hF6LiItqZwotEi-pZghzcxLsGf1N3p5P',
+    token_secret = 'ETi02p-5lO2hKM5bBACayb-cX_w'
 )
 client = Client(auth)
 
@@ -22,14 +22,16 @@ def index(request):
 
 def search(request):
 	if request.method == "POST":
+		latitude = float(request.POST['lati'])
+		longitude = float(request.POST['longi'])
 		params = {
-			'term': 'pizza',
-			'lang': 'en'
+			'sort_by' : 'rating',
+			'term' : 'pizza',
+			'lang' : 'en',
+			'radius_filter' : 2000
 		}
-		response = client.search('San Jose', **params)
-		print response.businesses[0].name
-		print response.businesses[1].name
-		print response.businesses[2].name
-
-	    return redirect('/')
-
+		response = client.search_by_coordinates(latitude, longitude, **params)
+	context = {
+				'businesses' : response.businesses
+	}
+	return render(request, 'hater_app/index.html', context)
